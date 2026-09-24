bbc9bf98332b8506bc0f15e5501a3886c8342137
gabrielcdr2 <gabrielcamposcdr@gmail.com>
2026-09-24 18:50:53 -0300

    Adicionado movimento e animação]

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    private CharacterController character;
+    private Animator animator;
+    private Vector3 inputs;
+
+    private float velocidade = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        character.Move(inputs * Time.deltaTime * velocidade);
+        character.Move(Vector3.down * Time.deltaTime);
+
+        if (inputs != Vector3.zero)
+        {
+            animator.SetBool("andando", true);
+            transform.forward = Vector3.Lerp(transform.forward, inputs, Time.deltaTime * 10f);
+        }
+        else
+        {
+            animator.SetBool("andando", false);
+        }
+    }
+}
